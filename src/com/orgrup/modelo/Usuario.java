0e0131eb0472c7bc19e9819172262dd70cd5d1ac
Daniel Rivera <danieldk8@gmail.com>
2012-06-08 15:36:37 -0400

    edicion modelo

---
+++ src/com/orgrup/modelo/Usuario.java
@@ -0,0 +1,21 @@
+package com.orgrup.modelo;
+
+
+
+public class Usuario {
+
+	private String correo;
+	private String nombre;
+	private String pass;
+	private Usuario usuario;
+	
+	public String getCorreo(){
+		
+		return correo;
+	}
+
+	public void setCorreo(){
+		this.correo = correo;
+	}
+	
+}
