aa7527204b66aed8b1c5973efdea7ff0661c24ab
Daniel Rivera <danieldk8@gmail.com>
2012-06-11 12:37:46 -0400

    estructura clase Documento

---
+++ src/com/orgrup/modelo/Documento.java
@@ -0,0 +1,35 @@
+package com.orgrup.modelo;
+
+public class Documento {
+	
+	private String fecha;
+	private String nombre;
+	private String ruta;
+		
+	public String getFecha() {
+		
+		return fecha;
+	}
+	
+	public void setFecha(String fecha) {
+		
+	}
+	
+	public String getNombre() {
+		
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		
+	}
+	
+	public String getRuta() {
+		
+		return ruta;
+	}
+	
+	public void setRuta(String ruta) {
+		
+	}
+}
